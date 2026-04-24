ca4ac0ebc7595d9d279f165feaa65072604a0057
Niklas Grieger <n.grieger@leuchtfeuer.com>
2026-04-24 09:08:06 +0200

    [TASK] Breaking #100963: Move form hooks from ext_tables.php to ext_localconf.php [TER-437] [TER-438]

--- ext_localconf.php
+++ ext_localconf.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 
+use Leuchtfeuer\Mautic\Hooks\MauticFormHook;
 use Leuchtfeuer\Mautic\Hooks\TCEmainHook;
 use TYPO3\CMS\Core\Core\Environment;
 use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
@@ -65,6 +66,16 @@ call_user_func(function (): void {
     // Register DataHandler hook for tag creation
     $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass'][] =
         TCEmainHook::class;
+
+    // Register form hooks for pushing forms to Mautic (moved from ext_tables.php)
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/form']['beforeFormDuplicate'][1489959059] =
+        MauticFormHook::class;
+
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/form']['beforeFormDelete'][1489959059] =
+        MauticFormHook::class;
+
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/form']['beforeFormSave'][1489959059] =
+        MauticFormHook::class;
 
     //##################
     //       FORM      #
--- ext_tables.php
+++ ext_tables.php
@@ -2,20 +2,4 @@
 
 declare(strict_types=1);
 
-use Leuchtfeuer\Mautic\Hooks\MauticFormHook;
 defined('TYPO3') || die;
-
-call_user_func(
-    function ($extensionKey): void {
-        // Assign the hooks for pushing newly created and edited forms to Mautic
-        $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/form']['beforeFormDuplicate'][1489959059] =
-            MauticFormHook::class;
-
-        $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/form']['beforeFormDelete'][1489959059] =
-            MauticFormHook::class;
-
-        $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/form']['beforeFormSave'][1489959059] =
-            MauticFormHook::class;
-    },
-    'mautic'
-);
